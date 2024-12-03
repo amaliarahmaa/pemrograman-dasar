8f41c17327e2285302cf7a998497443655b250e2
amaliarahmaa <rahma.amalia@mhs.itenas.ac.id>
2024-12-03 19:28:19 +0700

    soal 6

--- tugas.py
+++ tugas.py
@@ -76,7 +76,7 @@ print(f"Kebun C  Padi: {hasil_padi_lokasi3}, Kedelai: {hasil_kedelai_lokasi3}")
 print(f"Kebun D  Padi: {hasil_kedelai_lokasi4}, Kedelai: {hasil_kedelai_lokasi4}")
 print(f"Kebun E  Padi: {hasil_kedelai_lokasi5}, Kedelai: {hasil_kedelai_lokasi5}")
 
-#Membuat variabel terpisah untuk menyimpan jumlah hasil panen padi dan kedelai dari setiap lokasi.
+# Membuat variabel terpisah untuk menyimpan jumlah hasil panen padi dan kedelai dari setiap lokasi.
 kebun_a_padi = data_panen['lokasi1']['hasil_panen']['padi']
 kebun_b_padi = data_panen['lokasi2']['hasil_panen']['padi']
 kebun_c_padi = data_panen['lokasi3']['hasil_panen']['padi']
@@ -89,10 +89,21 @@ kebun_c_kedelai = data_panen['lokasi3']['hasil_panen']['kedelai']
 kebun_d_kedelai = data_panen['lokasi4']['hasil_panen']['kedelai']
 kebun_e_kedelai = data_panen['lokasi5']['hasil_panen']['kedelai']
 
-# Output hasil
 print("Hasil Panen Padi dan Kedelai dari Setiap Lokasi:")
 print(f"Kebun a Padi: {kebun_a_padi}, Kedelai: {kebun_a_kedelai}")
 print(f"Kebun b Padi: {kebun_b_padi}, Kedelai: {kebun_b_kedelai}")
 print(f"Kebun c Padi: {kebun_c_padi}, Kedelai: {kebun_c_kedelai}")
 print(f"Kebun d Padi: {kebun_d_padi}, Kedelai: {kebun_d_kedelai}")
 print(f"Kebun e Padi: {kebun_e_padi}, Kedelai: {kebun_e_kedelai}")
+
+# Percabangan
+for i, j in data_panen.items():
+    lokasi = j['nama_lokasi']
+    hasil = j['hasil_panen']
+    padi = hasil['padi']
+    jagung = hasil['jagung']
+
+    if padi > 1300 or jagung > 800:
+        print(f"Lokasi {lokasi} memerlukan perhatian khusus.")
+    else:
+        print(f"Lokasi {lokasi} dalam kondisi baik.")
