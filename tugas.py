64a3c602484b269e61156e2e1acc4b12a5ff236f
amaliarahmaa <rahma.amalia@mhs.itenas.ac.id>
2024-12-03 18:46:21 +0700

    soal 4

--- tugas.py
+++ tugas.py
@@ -53,6 +53,25 @@ hasil_lokasi2 = data_panen ['lokasi2'] ['hasil_panen'] ['jagung']
 print(f"Hasil panen dari lokasi 2 yaitu: {hasil_lokasi2}")
 
 # Menampilkan nama lokasi dari lokasi3
-
 nama_lokasi3 = data_panen ['lokasi3'] ['nama_lokasi']
 print(f"Nama lokasi dari lokasi3 yaitu: {nama_lokasi3}")
+
+# Memasukkan Jumlah Hasil Panen Padi dan Kedelai Setiap Lokasi ke Dalam Variabel yang Berbeda
+hasil_padi_lokasi1 = data_panen['lokasi1']['hasil_panen']['padi']
+hasil_padi_lokasi2 = data_panen['lokasi2']['hasil_panen']['padi']
+hasil_padi_lokasi3 = data_panen['lokasi3']['hasil_panen']['padi']
+hasil_padi_lokasi4 = data_panen['lokasi4']['hasil_panen']['padi']
+hasil_padi_lokasi5 = data_panen['lokasi5']['hasil_panen']['padi']
+
+hasil_kedelai_lokasi1 = data_panen['lokasi1']['hasil_panen']['kedelai']
+hasil_kedelai_lokasi2 = data_panen['lokasi2']['hasil_panen']['kedelai']
+hasil_kedelai_lokasi3 = data_panen['lokasi3']['hasil_panen']['kedelai']
+hasil_kedelai_lokasi4 = data_panen['lokasi4']['hasil_panen']['kedelai']
+hasil_kedelai_lokasi5 = data_panen['lokasi5']['hasil_panen']['kedelai']
+
+print("Jumlah hasil panen padi dan kedelai di lokasi yang berbeda:")
+print(f"Kebun A  Padi: {hasil_padi_lokasi1}, Kedelai: {hasil_kedelai_lokasi1}")
+print(f"Kebun B  Padi: {hasil_padi_lokasi2}, Kedelai: {hasil_kedelai_lokasi2}")
+print(f"Kebun C  Padi: {hasil_padi_lokasi3}, Kedelai: {hasil_kedelai_lokasi3}")
+print(f"Kebun D  Padi: {hasil_kedelai_lokasi4}, Kedelai: {hasil_kedelai_lokasi4}")
+print(f"Kebun E  Padi: {hasil_kedelai_lokasi5}, Kedelai: {hasil_kedelai_lokasi5}")
